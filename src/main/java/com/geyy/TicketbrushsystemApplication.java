22f89dad070d0e9711ae226eb024a3be653f08ca
geyaowei <759770542@qq.com>
2018-12-18 17:01:44 +0800

    加入连接池

--- src/main/java/com/geyy/TicketbrushsystemApplication.java
+++ src/main/java/com/geyy/TicketbrushsystemApplication.java
@@ -1,5 +1,6 @@
 package com.geyy;
 
+import com.geyy.ticketBruch.execute.Vote;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class TicketbrushsystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketbrushsystemApplication.class, args);
+		Vote vote = new Vote();
+		vote.vote();
 	}
 
 }
